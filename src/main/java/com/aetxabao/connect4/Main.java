12d48a063195167acde63d0181de46b6dd40d812
hrodriguezp <hrodrigpin@gmail.com>
2022-12-19 10:49:34 +0100

    prueba

--- src/main/java/com/aetxabao/connect4/Main.java
+++ src/main/java/com/aetxabao/connect4/Main.java
@@ -3,6 +3,7 @@ package com.aetxabao.connect4;
 /*
 mvn package
 /Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home/bin/jpackage --name Connect4 --icon src/main/resources/com/aetxabao/connect4/icon.icns --input target --main-jar Connect4-1.0-SNAPSHOT.jar --main-class com.aetxabao.connect4.Main
+prueba
  */
 public class Main {
     public static void main(String[] args) {
